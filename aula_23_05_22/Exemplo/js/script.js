1ff99104638cad504d2d8a8a0e89c05b92955891
Gabriel Oliveira Sampaio <gabrielsampaio1216@gmail.com>
2022-05-23 12:07:46 -0300

    Add files via upload

--- aula_23_05_22/Exemplo/js/script.js
+++ aula_23_05_22/Exemplo/js/script.js
@@ -6,14 +6,16 @@ var lamp = document.getElementById('lampada')
 botao.onclick = function(){
     //alert('funcionou')
     if(botao.value == 'Acender'){
-        lamp.src = './img/acesa.jpg'
+        lamp.src = './img/acesa.png'
+        botao.src = './img/ligar.png'
         botao.value = 'Apagar'
         botao.innerHTML = 'Apagar'
         document.body.style.backgroundColor = "royalblue"
         document.getElementById('qisso').innerHTML = "VAMOS ILUMINAR ISSO AQUI"
     }else{
-        lamp.src = './img/apagada.jpg'
+        lamp.src = './img/apagada.png'
         botao.value = 'Acender'
+        botao.src = './img/desligar.png'
         botao.innerHTML = 'Acender'
         document.body.style.backgroundColor = "darkblue"
         document.getElementById('qisso').innerHTML = "ESCURIDÃÃÃOOOO"
